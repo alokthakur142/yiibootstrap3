8b9fc5d27b053d2b210427dcd16a016c5990de49
DrMabuse <pascalbrewing@googlemail.com>
2013-10-30 18:52:54 +0100

    navbar examples

--- app/views/site/components/_navbar.php
+++ app/views/site/components/_navbar.php
@@ -317,20 +317,14 @@
 <h2 id="navbar-text">Text</h2>
 <p>Wrap strings of text in an element with <code>.navbar-text</code>, usually on a <code>&lt;p&gt;</code> tag for proper leading and color.</p>
 <div class="bs-example">
-    <nav class="navbar navbar-default" role="navigation">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex3-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Brand</a>
-        </div>
-        <div class="collapse navbar-collapse navbar-ex3-collapse">
-            <p class="navbar-text">Signed in as Mark Otto</p>
-        </div>
-    </nav>
+    <?php $this->widget('bootstrap.widgets.BsNavbar', array(
+            'collapse' => true,
+            'brandLabel' => 'Home',
+            'items'    => array(
+                BSHtml::menuText('Signed in as Mark Otto'),
+            ),
+        )
+    ); ?>
 </div>
 <div class="highlight"><pre><code class="html"><span class="nt">&lt;p</span> <span class="na">class=</span><span class="s">"navbar-text"</span><span class="nt">&gt;</span>Signed in as Mark Otto<span class="nt">&lt;/p&gt;</span>
         </code></pre></div>
@@ -339,20 +333,21 @@
 <h2 id="navbar-links">Non-nav links</h2>
 <p>For folks using standard links that are not within the regular navbar navigation component, use the <code>.navbar-link</code> class to add the proper colors for the default and inverse navbar options.</p>
 <div class="bs-example">
-    <nav class="navbar navbar-default" role="navigation">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex4-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Brand</a>
-        </div>
-        <div class="collapse navbar-collapse navbar-ex4-collapse">
-            <p class="navbar-text pull-right">Signed in as <a href="#" class="navbar-link">Mark Otto</a></p>
-        </div>
-    </nav>
+    <?php $this->widget('bootstrap.widgets.BsNavbar', array(
+            'collapse' => true,
+            'brandLabel' => 'Home',
+            'items'    => array(
+                BSHtml::menuText(
+                    'Signed in as '.BSHtml::link(
+                        'Mark Otto',
+                        array('site/user','id' => Yii::app()->user->id),
+                        array('class' => 'navbar-link')
+                    ),
+                    array('pull' => BSHtml::PULL_RIGHT)
+                ),
+            ),
+        )
+    ); ?>
 </div>
 <div class="highlight"><pre><code class="html"><span class="nt">&lt;p</span> <span class="na">class=</span><span class="s">"navbar-text pull-right"</span><span class="nt">&gt;</span>Signed in as <span class="nt">&lt;a</span> <span class="na">href=</span><span class="s">"#"</span> <span class="na">class=</span><span class="s">"navbar-link"</span><span class="nt">&gt;</span>Mark Otto<span class="nt">&lt;/a&gt;&lt;/p&gt;</span>
         </code></pre></div>
@@ -366,32 +361,29 @@
 <h2 id="navbar-fixed-top">Fixed to top</h2>
 <p>Add <code>.navbar-fixed-top</code>.</p>
 <div class="bs-example bs-navbar-top-example">
-    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex5-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Brand</a>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse navbar-ex5-collapse">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-            </ul>
-        </div><!-- /.navbar-collapse -->
-    </nav>
+    <?php $this->widget('bootstrap.widgets.BsNavbar', array(
+            'collapse' => true,
+            'position' => BSHtml::NAVBAR_POSITION_FIXED_TOP,
+            'brandLabel' => 'Home',
+            'items'    => array(
+                array(
+                    'class' => 'bootstrap.widgets.BsNav',
+                    'type'  => 'navbar',
+                    'activateParents' => true,
+                    'items' => array(
+                        array('label' => 'About', 'url' => array('/site/page', 'view' => 'about'),'active' => true),
+                        array('label' => 'Contact', 'url' => array('/site/contact')),
+                        array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
+                        array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
+                    ),
+                )
+            ),
+        )
+    ); ?>
 </div><!-- /example -->
-<div class="highlight"><pre><code class="html"><span class="nt">&lt;nav</span> <span class="na">class=</span><span class="s">"navbar navbar-default navbar-fixed-top"</span> <span class="na">role=</span><span class="s">"navigation"</span><span class="nt">&gt;</span>
-            ...
-            <span class="nt">&lt;/nav&gt;</span>
-        </code></pre></div>
+<div class="highlight">
+    <?= BSHtml::codeBlock("'position' => BSHtml::NAVBAR_POSITION_FIXED_TOP") ?>
+</div>
 
 <div class="bs-callout bs-callout-danger">
     <h4>Body padding required</h4>
@@ -405,38 +397,37 @@
 <h2 id="navbar-fixed-bottom">Fixed to bottom</h2>
 <p>Add <code>.navbar-fixed-bottom</code> instead.</p>
 <div class="bs-example bs-navbar-bottom-example">
-    <nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex6-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Brand</a>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse navbar-ex6-collapse">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-            </ul>
-        </div><!-- /.navbar-collapse -->
-    </nav>
+    <?php $this->widget('bootstrap.widgets.BsNavbar', array(
+            'collapse' => true,
+            'position' => BSHtml::NAVBAR_POSITION_FIXED_BOTTOM,
+            'brandLabel' => 'Home',
+            'items'    => array(
+                array(
+                    'class' => 'bootstrap.widgets.BsNav',
+                    'type'  => 'navbar',
+                    'activateParents' => true,
+                    'items' => array(
+                        array('label' => 'About', 'url' => array('/site/page', 'view' => 'about'),'active' => true),
+                        array('label' => 'Contact', 'url' => array('/site/contact')),
+                        array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
+                        array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
+                    ),
+                )
+            ),
+        )
+    ); ?>
 </div><!-- /example -->
-<div class="highlight"><pre><code class="html"><span class="nt">&lt;nav</span> <span class="na">class=</span><span class="s">"navbar navbar-default navbar-fixed-bottom"</span> <span class="na">role=</span><span class="s">"navigation"</span><span class="nt">&gt;</span>
-            ...
-            <span class="nt">&lt;/nav&gt;</span>
-        </code></pre></div>
+<div class="highlight">
+    <?= BSHtml::codeBlock("'position' => BSHtml::NAVBAR_POSITION_FIXED_BOTTOM") ?>
+</div>
 
 <div class="bs-callout bs-callout-danger">
     <h4>Body padding required</h4>
     <p>The fixed navbar will overlay your other content, unless you add <code>padding</code> to the bottom of the <code>&lt;body&gt;</code>. Try out your own values or use our snippet below. Tip: By default, the navbar is 50px high.</p>
-    <div class="highlight"><pre><code class="css"><span class="nt">body</span> <span class="p">{</span> <span class="k">padding-bottom</span><span class="o">:</span> <span class="m">70px</span><span class="p">;</span> <span class="p">}</span>
-            </code></pre></div>
+    <div class="highlight">
+        <pre><code class="css"><span class="nt">body</span> <span class="p">{</span> <span class="k">padding-bottom</span><span class="o">:</span> <span class="m">70px</span><span class="p">;</span> <span class="p">}</span>
+            </code></pre>
+    </div>
     <p>Make sure to include this <strong>after</strong> the core Bootstrap CSS.</p>
 </div>
 
@@ -444,62 +435,56 @@
 <h2 id="navbar-static-top">Static top</h2>
 <p>Create a full-width navbar that scrolls away with the page by adding <code>.navbar-static-top</code>. Unlike the <code>.navbar-fixed-*</code> classes, you do not need to change any padding on the <code>body</code>.</p>
 <div class="bs-example bs-navbar-top-example">
-    <nav class="navbar navbar-default navbar-static-top" role="navigation">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex7-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Brand</a>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse navbar-ex7-collapse">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-            </ul>
-        </div><!-- /.navbar-collapse -->
-    </nav>
+    <?php $this->widget('bootstrap.widgets.BsNavbar', array(
+            'collapse' => true,
+            'position' => BSHtml::NAVBAR_POSITION_STATIC_TOP,
+            'brandLabel' => 'Home',
+            'items'    => array(
+                array(
+                    'class' => 'bootstrap.widgets.BsNav',
+                    'type'  => 'navbar',
+                    'activateParents' => true,
+                    'items' => array(
+                        array('label' => 'About', 'url' => array('/site/page', 'view' => 'about'),'active' => true),
+                        array('label' => 'Contact', 'url' => array('/site/contact')),
+                        array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
+                        array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
+                    ),
+                )
+            ),
+        )
+    ); ?>
 </div><!-- /example -->
-<div class="highlight"><pre><code class="html"><span class="nt">&lt;nav</span> <span class="na">class=</span><span class="s">"navbar navbar-default navbar-static-top"</span> <span class="na">role=</span><span class="s">"navigation"</span><span class="nt">&gt;</span>
-            ...
-            <span class="nt">&lt;/nav&gt;</span>
-        </code></pre></div>
+<div class="highlight">
+    <?= BSHtml::codeBlock("'position' => BSHtml::NAVBAR_POSITION_STATIC_TOP") ?>
+</div>
 
 
 <h2 id="navbar-inverted">Inverted navbar</h2>
 <p>Modify the look of the navbar by adding <code>.navbar-inverse</code>.</p>
 <div class="bs-example">
-    <nav class="navbar navbar-inverse" role="navigation">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex8-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Brand</a>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse navbar-ex8-collapse">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Link</a></li>
-                <li><a href="#">Link</a></li>
-            </ul>
-        </div><!-- /.navbar-collapse -->
-    </nav>
+    <?php $this->widget('bootstrap.widgets.BsNavbar', array(
+            'collapse' => true,
+            'color' => BSHtml::NAVBAR_COLOR_INVERSE,
+            'brandLabel' => 'Home',
+            'items'    => array(
+                array(
+                    'class' => 'bootstrap.widgets.BsNav',
+                    'type'  => 'navbar',
+                    'activateParents' => true,
+                    'items' => array(
+                        array('label' => 'About', 'url' => array('/site/page', 'view' => 'about'),'active' => true),
+                        array('label' => 'Contact', 'url' => array('/site/contact')),
+                        array('label' => 'Login', 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
+                        array('label' => 'Logout (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
+                    ),
+                )
+            ),
+        )
+    ); ?>
 </div><!-- /example -->
-<div class="highlight"><pre><code class="html"><span class="nt">&lt;nav</span> <span class="na">class=</span><span class="s">"navbar navbar-inverse"</span> <span class="na">role=</span><span class="s">"navigation"</span><span class="nt">&gt;</span>
-            ...
-            <span class="nt">&lt;/nav&gt;</span>
-        </code></pre></div>
+<div class="highlight">
+    <?= BSHtml::codeBlock("'color' => BSHtml::NAVBAR_COLOR_INVERSE") ?>
+</div>
 
 </div>
